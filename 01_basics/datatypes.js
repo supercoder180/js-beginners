f825a02a7bf506933d719463741ad2c533ef7c4d
supercoder180 <ntuser090@gmail.com>
2024-09-13 16:19:43 +0000

    updated

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+
+let name = "Hitesh" // string variable
+let age = 18 //number
+let isLogedIn = true
+
+
+/* Primitive Data types 
+
+-----------------------------------
+// Number -> 2 to power53 
+//bigInt 
+//string =>""
+//boolean => true/false
+//null -> it is a standalone value as well as datatype. Null is also basically representation of Empty value. 
+//undefined-> when value is not assigned.
+// symbol => unquieness
+---------------------------------
+Non-Primitive data types
+
+//objects
+*/
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null);
+
+
+
+
+
+
+
