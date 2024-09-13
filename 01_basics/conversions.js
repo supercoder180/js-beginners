8e31835da85c090af299b6b7402a2f509d1b44ce
supercoder180 <ntuser090@gmail.com>
2024-09-13 16:58:45 +0000

    conversions of datatypes

---
+++ 01_basics/conversions.js
@@ -0,0 +1,42 @@
+//let score = "33abc"//
+//let score = null//
+/*let score = undefined
+
+
+console.log(typeof score);
+console.log(typeof(score)); // written as a method
+
+let valueINNumber = Number(score) // now score is converted in Number
+console.log(typeof valueINNumber);
+console.log(score);
+console.log(valueINNumber);*/
+
+
+
+
+// Now we know that the value received is in the form of string but we want to per
+//form the oprn on numbers so we will have to convert them.
+
+//let isLogedIn = 1
+// when let isLogedIn is assigned as string and empty string.
+
+//let isLogedIn ="Hitesh"
+ //let isLogedIn = null
+ //let isLogedIn =undefined
+
+//let booleanisLogedIn = Boolean(isLogedIn)
+//console.log(booleanisLogedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber) // syntax to change string.
+console.log(typeof stringNumber);
+
+
+
+
+
+
+
+
+
