1f63485afe4907a1dd01454466ef0c0645d9d21b
supercoder180 <ntuser090@gmail.com>
2024-09-15 12:38:11 +0000

    strings updated

---
+++ 01_basics/strings.js
@@ -0,0 +1,40 @@
+// string can be annotated with both "" and ''
+
+const name = "hitesh"
+const repocount = 50
+//console.log(name + repocount +"value"); outdated syntax
+
+//console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gameName = new String('hiteshhc')
+// console.log(typeof gameName);
+// console.log(typeof name);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(0,4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
