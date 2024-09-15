2a7e1b0c176f4285d77a4948cdb19c6de433d3a7
supercoder180 <ntuser090@gmail.com>
2024-09-15 13:08:30 +0000

    done

---
+++ 01_basics/nums-maths.js
@@ -0,0 +1,26 @@
+const score =400
+console.log(score);
+
+const balance = new Number(100) // new object is defined as number 
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 23.89444
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+const min = 10
+const max = 20
+
+Math.random() * (max - min +1)
+
+
+
+
+
+
+
