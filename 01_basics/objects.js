ed8d15de2ddfed2e661f26577bb4b93bee9081f0
supercoder180 <ntuser090@gmail.com>
2024-09-16 05:59:25 +0000

    objects

--- 01_basics/objects.js
+++ 01_basics/objects.js
@@ -14,11 +14,28 @@ const Jsuser = {
 
 }
 
-console.log(Jsuser.email);// common ways to access objects.
-console.log(Jsuser["email"]);
-console.log(Jsuser.mySym);
-console.log(typeof mySym);
-console.log(typeof Jsuser.mySym);
+// console.log(Jsuser.email);// common ways to access objects.
+// console.log(Jsuser["email"]);
+// console.log(Jsuser.mySym);
+// console.log(typeof mySym);
+// console.log(typeof Jsuser.mySym);
+
+Jsuser.email ="hitesh@chatgpt.com"
+//Object.freeze(Jsuser)// changes will not propogate for that object
+
+Jsuser.greeting = function(){
+    
+    console.log("Hello Jsuser ");
+    
+}
+Jsuser.greetingTwo = function(){
+    console.log(`Hello js user, ${this.name}`);
+}
+
+console.log(Jsuser.greeting());
+
+
+
 
 
 
