0af71c534bb39c51bc5f3e1c3b21efdbeec1c8b4
supercoder180 <ntuser090@gmail.com>
2024-09-16 16:30:57 +0000

    functions

---
+++ 01_basics/functions_01.js
@@ -0,0 +1,36 @@
+
+function sayMyname(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+
+}
+//sayMyname()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+    
+}
+
+addTwoNumbers(3,null) // here refrence is being passed into the function
+
+// while defining functions whatever input is being taken is treated as paramters
+// and when value is being passed while calling the functions is termed as arguments.
+
+// So in our case 
+// function addTwoNumbers(number1, number2) here num1 and num2 is considered as parameters
+
+// and addTwoNumbers(3,4) termed as passing the arguments.
+
+const result = addTwoNumbers(3,5)
+console.log();
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 +number2
+    // console.log("hitesh");
+    // return result
+    return number1 + number2
+}
+
+function  
+
